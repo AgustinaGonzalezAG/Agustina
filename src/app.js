98ca2e5f03a8ba1a6660f545a8ecda6db30a6584
AgustinaGonzalezAG <139406447+AgustinaGonzalezAG@users.noreply.github.com>
2023-10-27 01:41:27 +0000

    [terminado]

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //evento al boton
+  let boton = document.getElementById("nuevaCarta");
+
+  //genera numeros aleatorios y los guarda en una variable
+  function generarValoresAleatorios() {
+    let palos = [" ♦ ", "♥", "♠", "♣"];
+    let valor = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+    let paloAleatorio = palos[Math.floor(Math.random() * palos.length)]; //me da del 1 al 4?
+    let valorAlatorio = valor[Math.floor(Math.random() * valor.length)];
+
+    let numero = document.getElementById("nuevoNumero");
+    let palo = document.getElementsByClassName("palo");
+    numero.innerHTML = valorAlatorio;
+    palo[0].innerHTML = paloAleatorio;
+    palo[1].innerHTML = paloAleatorio;
+  }
+
+  boton.addEventListener("click", generarValoresAleatorios);
+  //ahora generar una nueva carta
 };
